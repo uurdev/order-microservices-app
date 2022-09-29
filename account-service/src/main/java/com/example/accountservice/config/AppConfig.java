c51acdce49e28753ed816006773a01119e07c118
Uğur Çiftçi <45206582+uurdev@users.noreply.github.com>
2022-09-29 20:32:34 +0300

    add account service

---
+++ account-service/src/main/java/com/example/accountservice/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.accountservice.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/*
+    @author uurdev
+    @date 29.09.2022
+*/
+@EnableAutoConfiguration
+@Configuration
+public class AppConfig {
+}
