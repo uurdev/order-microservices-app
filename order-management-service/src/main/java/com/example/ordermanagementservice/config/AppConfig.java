11b7cdd9d7967105fc852d43ccaa014286d98455
Uğur Çiftçi <45206582+uurdev@users.noreply.github.com>
2022-10-01 19:59:59 +0300

    add order management service

---
+++ order-management-service/src/main/java/com/example/ordermanagementservice/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.ordermanagementservice.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/*
+    @author uurdev
+    @date 1.10.2022
+*/
+@Configuration
+@EnableAutoConfiguration
+public class AppConfig {
+}
