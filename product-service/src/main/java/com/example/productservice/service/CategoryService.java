82f8d3d2fda3f4011306d0821093d8d45d02333d
Uğur Çiftçi <45206582+uurdev@users.noreply.github.com>
2022-10-04 23:46:48 +0300

    add category service

--- product-service/src/main/java/com/example/productservice/service/CategoryService.java
+++ product-service/src/main/java/com/example/productservice/service/CategoryService.java
@@ -5,5 +5,9 @@ package com.example.productservice.service;
     @date 2.10.2022
 */
 
+import com.example.productservice.domain.Category;
+
 public interface CategoryService {
+    Category save(Category category);
+    Category getById(String id);
 }
--- product-service/src/main/java/com/example/productservice/service/impl/CategoryServiceImpl.java
+++ product-service/src/main/java/com/example/productservice/service/impl/CategoryServiceImpl.java
@@ -6,6 +6,8 @@ package com.example.productservice.service.impl;
 */
 
 
+import com.example.productservice.domain.Category;
+import com.example.productservice.repository.CategoryRepository;
 import com.example.productservice.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,4 +17,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category getById(String id) {
+        return categoryRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
 }
