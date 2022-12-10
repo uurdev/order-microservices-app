d38dc5842bde70b38855c71a5582fd68d57813c0
Uğur Çiftçi <45206582+uurdev@users.noreply.github.com>
2022-12-10 13:09:36 +0300

    add banner

--- order-management-service/src/main/java/com/example/ordermanagementservice/OrderManagementServiceApplication.java
+++ order-management-service/src/main/java/com/example/ordermanagementservice/OrderManagementServiceApplication.java
@@ -11,5 +11,4 @@ public class OrderManagementServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderManagementServiceApplication.class, args);
     }
-
 }
